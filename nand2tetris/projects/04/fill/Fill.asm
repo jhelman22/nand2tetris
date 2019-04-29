8d91243351387a4943ba1f6663141a2c01a9e8dc
Jake Helman <jacob.helman@hbdi.com>
2019-04-29 11:22:04 -0400

    Project 4

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,63 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Infinite loop
+  // check if kbd is not zero
+  // if not zero, write -1 in all pixels
+  // if zero, write 0 in all pixels
+
+  @8192 // number of words in screen memory map
+  D=A
+  @t // set to variable t
+  M=D
+
+(LOOP)
+  @i // offset for screen loop
+  M=0
+
+  @KBD
+  D=M
+  @FILL
+  D; JGT
+  @CLEAR
+  D; JEQ
+  
+(FILL)
+  @i
+  D=M
+  @t
+  D=M-D
+  @LOOP
+  D; JEQ // exit fill loop once offset is 512 (ie. all of screen has been filled)
+
+  @i
+  D=M
+  @SCREEN
+  A=A+D // set address to SCREEN + offset
+  M=-1
+
+  @i
+  M=M+1
+  
+  @FILL
+  0; JMP
+
+(CLEAR)
+  @i
+  D=M
+  @t
+  D=M-D
+  @LOOP
+  D; JEQ // exit fill loop once offset is 512 (ie. all of screen has been cleared)
+
+  @i
+  D=M
+  @SCREEN
+  A=A+D // set address to SCREEN + offset
+  M=0
+
+  @i
+  M=M+1
+
+  @CLEAR
+  0; JMP
